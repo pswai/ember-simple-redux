83208d4c9b4ae883dd2b4013875cada829ec2c6d
PSWai <parksoon_my@hotmail.com>
2018-07-25 17:41:18 +0800

    Add handling of `mergeProps`

--- addon/connect.js
+++ addon/connect.js
@@ -1,22 +1,50 @@
+const defaultMergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...ownProps,
+  ...stateProps,
+  ...dispatchProps,
+});
+
 const connect = (
-  mapStateToProps
-  // mapDispatchToProps,
-  // mergeProps,
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
   // options
 ) => EmberComponent => {
   const update = componentInstance => {
     const { simpleReduxStore, attrs } = componentInstance;
     const state = simpleReduxStore.getState();
-    let stateProps;
+    let stateProps, dispatchProps, finalProps;
+    let ownProps = componentInstance.getProperties(Object.keys(attrs));
 
     // Check arity, if arity is 1 then no `ownProps` is needed
-    if (mapStateToProps.length !== 1) {
-      const ownProps = componentInstance.getProperties(Object.keys(attrs));
+    if (!mapStateToProps) {
+      stateProps = {};
+    } else if (mapStateToProps.length !== 1) {
       stateProps = mapStateToProps(state, ownProps);
     } else {
       stateProps = mapStateToProps(state);
     }
-    componentInstance.setProperties(stateProps);
+
+    dispatchProps = mapDispatchToProps;
+
+    // Do mergeProps
+    if (mergeProps) {
+      // Create an object with all existing attrs set to undefined.
+      // This prevents attrs to be leaked to component if `mergeProps` does not
+      // define it.
+      const resetProps = Object.keys(attrs).reduce((result, key) => {
+        result[key] = undefined;
+        return result;
+      }, {});
+      finalProps = {
+        ...resetProps,
+        ...mergeProps(stateProps, dispatchProps, ownProps),
+      };
+    } else {
+      finalProps = defaultMergeProps(stateProps, dispatchProps, ownProps);
+    }
+
+    componentInstance.setProperties(finalProps);
   };
 
   return EmberComponent.extend({
@@ -24,9 +52,10 @@ const connect = (
       const store = this.simpleReduxStore;
 
       if (mapStateToProps) {
-        update(this);
         this.unsubscribe = store.subscribe(() => update(this));
       }
+
+      update(this);
       this._super(...arguments);
     },
 
