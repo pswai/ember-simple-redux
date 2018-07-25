d588eec23442f3648666f524730342faa1c11384
PSWai <parksoon_my@hotmail.com>
2018-07-25 17:41:28 +0800

    Add tests to cover lots of cases

--- tests/integration/connect-test.js
+++ tests/integration/connect-test.js
@@ -24,7 +24,21 @@ module('Integration | connect', function(hooks) {
     );
   });
 
-  test('it calls `mapStateToProps` with Redux state (arity: 0)', async function(assert) {
+  test('it passes original props to connected component', async function(assert) {
+    const connectedComponent = connect()(ClickForInstance);
+    this.owner.register('component:test-target', connectedComponent);
+
+    const spy = sinon.spy();
+    this.set('spy', spy);
+    await render(hbs`{{test-target onClick=spy foo=1 bar='test'}}`);
+    await click('.test-target');
+
+    const instance = spy.getCall(0).args[0];
+    assert.equal(instance.get('foo'), 1);
+    assert.equal(instance.get('bar'), 'test');
+  });
+
+  test('mapStateToProps: called with Redux state and `ownProps` when arity 0', async function(assert) {
     const state = {};
     const store = createStore(() => state);
     this.owner.register('simple-redux:store', store);
@@ -35,14 +49,14 @@ module('Integration | connect', function(hooks) {
     const connectedComponent = connect(spy)(Component);
     this.owner.register('component:test-target', connectedComponent);
 
-    await render(hbs`{{test-target foo='1'}}`);
+    await render(hbs`{{test-target foo=1}}`);
 
     const { args } = spy.getCall(0);
     assert.equal(args[0], state, '`state` should be as is');
-    assert.deepEqual(args[1], { foo: '1' }, '`ownProps` should be passed');
+    assert.deepEqual(args[1], { foo: 1 }, '`ownProps` should be passed');
   });
 
-  test('it calls `mapStateToProps` with Redux state (arity: 1)', async function(assert) {
+  test('mapStateToProps: called with Redux state only when arity 1', async function(assert) {
     const state = {};
     const store = createStore(() => state);
     this.owner.register('simple-redux:store', store);
@@ -60,7 +74,7 @@ module('Integration | connect', function(hooks) {
     assert.equal(args.length, 1, '`ownProps` should not be passed');
   });
 
-  test('it calls `mapStateToProps` with Redux state (arity: 2)', async function(assert) {
+  test('mapStateToProps: called with Redux state and `ownProps` when arity 2', async function(assert) {
     const state = {};
     const store = createStore(() => state);
     this.owner.register('simple-redux:store', store);
@@ -71,43 +85,151 @@ module('Integration | connect', function(hooks) {
     const connectedComponent = connect(spy)(Component);
     this.owner.register('component:test-target', connectedComponent);
 
-    await render(hbs`{{test-target foo='1'}}`);
+    await render(hbs`{{test-target foo=1}}`);
 
     const { args } = spy.getCall(0);
     assert.equal(args[0], state, '`state` should be as is');
-    assert.deepEqual(args[1], { foo: '1' }, '`ownProps` should be passed');
+    assert.deepEqual(args[1], { foo: 1 }, '`ownProps` should be passed');
   });
 
-  test('it passes original props to connected component', async function(assert) {
-    const connectedComponent = connect()(ClickForInstance);
-    this.owner.register('component:test-target', connectedComponent);
-
-    const spy = sinon.spy();
-    this.set('spy', spy);
-    await render(hbs`{{test-target onClick=spy foo='1' bar='test'}}`);
-    await click('.test-target');
-
-    const instance = spy.getCall(0).args[0];
-    assert.equal(instance.get('foo'), '1');
-    assert.equal(instance.get('bar'), 'test');
-  });
-
-  test('it sets result of `mapStateToProps` to connected component', async function(assert) {
+  test('mapStateToProps: it sets `stateProps` to connected component', async function(assert) {
     const store = createStore(() => ({
       count: 5,
     }));
     this.owner.register('simple-redux:store', store);
 
-    const mapStateToProps = ({ count }) => ({ count });
+    const mapStateToProps = (state, ownProps) => ({
+      count: state.count,
+      fooPlusCount: state.count + ownProps.foo,
+    });
     const connectedComponent = connect(mapStateToProps)(ClickForInstance);
     this.owner.register('component:test-target', connectedComponent);
 
     const spy = sinon.spy();
     this.set('spy', spy);
-    await render(hbs`{{test-target onClick=spy foo='1' bar='test'}}`);
+    await render(hbs`{{test-target onClick=spy foo=1 bar='test'}}`);
     await click('.test-target');
 
     const instance = spy.getCall(0).args[0];
     assert.equal(instance.get('count'), 5);
+    assert.equal(instance.get('fooPlusCount'), 6);
+    assert.equal(instance.get('bar'), 'test', '`bar` should be still there');
+  });
+
+  test('default mergeProps: `stateProps` overrides `ownProps`', async function(assert) {
+    const store = createStore(() => ({
+      count: 5,
+    }));
+    this.owner.register('simple-redux:store', store);
+
+    const mapStateToProps = ({ count }) => ({ foo: count });
+    const connectedComponent = connect(mapStateToProps)(ClickForInstance);
+    this.owner.register('component:test-target', connectedComponent);
+
+    const spy = sinon.spy();
+    this.set('spy', spy);
+    await render(hbs`{{test-target onClick=spy foo=1 bar='test'}}`);
+    await click('.test-target');
+
+    const instance = spy.getCall(0).args[0];
+    assert.equal(instance.get('foo'), 5);
+    assert.equal(instance.get('bar'), 'test', '`bar` should be still there');
+  });
+
+  test('default mergeProps: `dispatchProps` overrides `ownProps`', async function(assert) {
+    const mapDispatchToProps = { foo() {} };
+    const connectedComponent = connect(
+      null,
+      mapDispatchToProps
+    )(ClickForInstance);
+    this.owner.register('component:test-target', connectedComponent);
+
+    const spy = sinon.spy();
+    this.set('spy', spy);
+    await render(hbs`{{test-target onClick=spy foo=1 bar='test'}}`);
+    await click('.test-target');
+
+    const instance = spy.getCall(0).args[0];
+    assert.equal(typeOf(instance.get('foo')), 'function'); // The bound action creator
+    assert.equal(instance.get('bar'), 'test', '`bar` should be still there');
+  });
+
+  test('default mergeProps: `dispatchProps` overrides `stateProps`', async function(assert) {
+    const store = createStore(() => ({
+      count: 5,
+    }));
+    this.owner.register('simple-redux:store', store);
+    const mapStateToProps = ({ count }) => ({ foo: count });
+    const mapDispatchToProps = { foo() {} };
+    const connectedComponent = connect(
+      mapStateToProps,
+      mapDispatchToProps
+    )(ClickForInstance);
+    this.owner.register('component:test-target', connectedComponent);
+
+    const spy = sinon.spy();
+    this.set('spy', spy);
+    await render(hbs`{{test-target onClick=spy bar='test'}}`);
+    await click('.test-target');
+
+    const instance = spy.getCall(0).args[0];
+    assert.equal(typeOf(instance.get('foo')), 'function'); // The bound action creator
+    assert.equal(instance.get('bar'), 'test', '`bar` should be still there');
+  });
+
+  test('mergeProps: result set to connected component', async function(assert) {
+    const store = createStore(() => ({
+      count: 5,
+    }));
+    this.owner.register('simple-redux:store', store);
+
+    const mapStateToProps = ({ count }) => ({ foo: count });
+    const mapDispatchToProps = { foo() {} };
+    const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+      stateFoo: stateProps.foo,
+      dispatchFoo: dispatchProps.foo,
+      ownFoo: ownProps.foo,
+      onClick: ownProps.onClick,
+    });
+    const connectedComponent = connect(
+      mapStateToProps,
+      mapDispatchToProps,
+      mergeProps
+    )(ClickForInstance);
+    this.owner.register('component:test-target', connectedComponent);
+
+    const spy = sinon.spy();
+    this.set('spy', spy);
+    await render(hbs`{{test-target onClick=spy foo=1 bar='test'}}`);
+    await click('.test-target');
+
+    const instance = spy.getCall(0).args[0];
+    assert.equal(instance.get('stateFoo'), 5);
+    assert.equal(typeOf(instance.get('dispatchFoo')), 'function'); // The bound action creator
+    assert.equal(instance.get('ownFoo'), 1);
+    assert.equal(instance.get('foo'), undefined, '`foo` should be undefined');
+    assert.equal(instance.get('bar'), undefined, '`bar` should be undefined');
+  });
+
+  test('mergeProps: prevents leaking props (only the result will be passed to component)', async function(assert) {
+    const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+      onClick: ownProps.onClick,
+    });
+    const connectedComponent = connect(
+      null,
+      null,
+      mergeProps
+    )(ClickForInstance);
+    this.owner.register('component:test-target', connectedComponent);
+
+    const spy = sinon.spy();
+    this.set('spy', spy);
+    await render(hbs`{{test-target onClick=spy foo=1 bar='test'}}`);
+    await click('.test-target');
+
+    const instance = spy.getCall(0).args[0];
+    assert.equal(typeOf(instance.get('onClick')), 'function');
+    assert.equal(instance.get('foo'), undefined, '`foo` should be undefined');
+    assert.equal(instance.get('bar'), undefined, '`bar` should be undefined');
   });
 });
