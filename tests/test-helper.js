81715b505eaf6f4976acdefed54cd894c100b481
PSWai <parksoon_my@hotmail.com>
2018-07-30 14:13:14 +0800

    Add `sinon-chai`

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,9 @@
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
+
+chai.use(sinonChai);
 
 setApplication(Application.create(config.APP));
