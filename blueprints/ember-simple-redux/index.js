af1bcbb0d3411ba5d1fb11f01770e563e60cb00d
PSWai <parksoon_my@hotmail.com>
2018-08-01 23:23:37 +0800

    Override `normalizeEntityName` in blueprints to prevent error without entity name

--- blueprints/ember-simple-redux/index.js
+++ blueprints/ember-simple-redux/index.js
@@ -2,6 +2,8 @@
 module.exports = {
   description: 'Install dependencies and blueprints',
 
+  normalizeEntityName: function() {},
+
   afterInstall() {
     return this.addPackageToProject('ember-auto-import');
   },
