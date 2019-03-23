f2864cc8004349c3eb901cb8d31338ece4a246de
PSWai <parksoon_my@hotmail.com>
2019-03-23 23:08:02 +0800

    Test semver

--- addon/utils/ember-version-info.js
+++ addon/utils/ember-version-info.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import semver from 'semver';
 
 const emberVersionInfo = () => {
   const [major, minor] = Ember.VERSION.split('.');
-  const isGlimmer = major > 2 || (major == 2 && minor >= 10); // >= 2.10
+  // const isGlimmer = major > 2 || (major == 2 && minor >= 10); // >= 2.10
+  const isGlimmer = semver.gte(Ember.VERSION, '2.10');
   return { major, minor, isGlimmer };
 };
 
